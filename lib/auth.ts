581c6a630a7811ea99db62c59dbd1a138ef964d9
Forhad Reza <forhad.bimt@gmail.com>
2026-01-29 23:53:03 +0600

    refactor: clean up auth schema and set default image for user

--- lib/auth.ts
+++ lib/auth.ts
@@ -37,8 +37,6 @@ export const auth = betterAuth({
          image: {
             type: 'string',
             required: false,
-            defaultValue:
-               'https://res.cloudinary.com/dqs6k0so6/image/upload/v1769692289/profile_eguhty.png',
             input: true,
          },
       },
